d62f583d2fce4e59fbbe1c1b5b4745bc2fc29069
Gabriel-lct <gabriellancelot1@gmail.com>
2025-11-22 01:18:23 +0100

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

--- cpp/metropolis.hpp
+++ cpp/metropolis.hpp
@@ -7,10 +7,10 @@ using configuration = std::vector<std::vector<int>>;
 void init_configuration(configuration &config);
 
 // Define the energy function
-double energy(const configuration &, double J);
+double energy(const configuration &, double J, double B);
 
 // Define the delta energy function
-double delta_energy(const configuration &config, double J, int x, int y);
+double delta_energy(const configuration &config, double J, double B, int x, int y);
 
 // Define the powerful metropolis function
-std::vector<configuration> metropolis(configuration config, double J, double T, int nb_iteration, int nb_intermediate_config);
+std::vector<configuration> metropolis(configuration config, double J, double B, double T, int nb_iteration, int nb_intermediate_config);
